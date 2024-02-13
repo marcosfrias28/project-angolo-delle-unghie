f3814a68dfeda4822b2d2ab9cafa47535f8ae29d
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-02-13 14:17:43 +0100

    New updates

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,7 +7,7 @@ import SocialCard from "./Social Network/SocialCard.jsx";
 function Contacts() {
     return (
         <section id="Contattami" className="bg-cover bg-no-repeat bg-center Home w-full h-screen xl:max-h-svh grid grid-cols-2 place-content-center place-items-center">
-        <section className="w-3/4 h-full xl:max-h-svh flex flex-col place-self-end">
+        <section className="w-3/4 h-full xl:max-h-svh flex flex-col place-self-end flex-wrap">
             <SocialCard name="Instagram" link="https://www.instagram.com/angolodelleunghie_/" children={<InstagramIcon className="" />} />
             <SocialCard name="Facebook" link="" children={<FacebookIcon className="" />} />
             <SocialCard name="Whatsapp" link="" children={<WhatsappIcon className="" />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header id='header' className='fixed z-10 w-full'>
     <div id='nav' className={`shadow-xl flex flex-row flex-wrap justify-center items-center w-screen m-2xl h-12 px-12 bg-fair-pink-200`}>
-      <nav  className='w-full flex flex-row flex-nowrap gap-14 justify-center items-center h-auto text-sm xl:max-w-2xl'>
+      <nav  className='w-full flex flex-row flex-nowrap gap-6 justify-center items-center h-auto text-sm xl:max-w-2xl lg:gap-14'>
           {
             NavBar.map((name, index) => (<a key={index} className='hover:bg-fair-pink-100 hover:py-3 p-1 font-semibold' href={'#'.concat(name).replace(' ', '-')}>{name}</a>))
           }
