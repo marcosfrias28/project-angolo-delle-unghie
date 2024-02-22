178a48c3b14b65b87c5016173d62c725bfad4504
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-02-22 17:38:33 +0100

    Responsive adjustments

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,13 +6,13 @@ import SocialCard from "./Social Network/SocialCard.jsx";
 
 function Contacts() {
     return (
-        <section id="Contattami" className="bg-cover bg-no-repeat bg-center Home w-full h-screen xl:max-h-svh flex place-content-center place-items-center max-[360px]:flex sm: sm:flex-col sm:flex-wrap">
+        <section id="Contattami" className="bg-cover bg-no-repeat bg-center Home w-full h-screen xl:max-h-svh flex place-content-center place-items-center max-[360px]:flex sm:flex-col sm:flex-wrap">
         <section className="w-3/4 h-screen z-10 flex flex-col flex-wrap place-content-center place-item-center">
             <SocialCard name="Instagram" link="https://www.instagram.com/angolodelleunghie_/" children={<InstagramIcon className="" />} />
             <SocialCard name="Facebook" link="" children={<FacebookIcon className="" />} />
             <SocialCard name="Whatsapp" link="" children={<WhatsappIcon className="" />} />
         </section>
-        <section className="absolute h-auto w-96 opacity-10">
+        <section className="absolute h-screen w-screen opacity-5">
              <Logo width={"100%"} height={"100%"}/>
         </section>
       </section>
